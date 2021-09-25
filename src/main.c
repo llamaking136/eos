6543d949a5243abc9764470af7e4e4ce97ea0411
llamaking136 <67032575+llamaking136@users.noreply.github.com>
2021-09-24 19:33:58 -0600

    Edited the error message during file reads. Main now calls `usage()` when no arguments are provided. Also did some tidying up.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tokenizer.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int is_dir(const char *cc)
+{
+	struct stat buffer;
+	stat(cc, &buffer);
+	return S_ISDIR(buffer.st_mode);
+}
+
+int file_exists(const char *cc)
+{
+	struct stat buffer;
+	return stat(cc, &buffer) == 0;
+}
+
+void error_read(const char *name)
+{
+	if (is_dir(name)) 
+	{
+		log_error("Cannot read file '%s'! Error: Is a Directory", name);
+	} 
+	else if (!file_exists(name))
+	{
+		log_error("Cannot read file '%s'! Error: File not Found", name);
+	}
+	else
+	{
+		log_error("Cannot read file '%s'! Errno: [%d]", name, errno);
+	}
+}
 
 void usage(FILE *stream, char *invocation)
 {
@@ -14,7 +45,7 @@ void usage(FILE *stream, char *invocation)
     fprintf(stream, "	-h, --help		Show this message\n");
 }
 
-// utility function to go through command line arguments.
+/* utility function to go through command line arguments. */
 char *shift(int *argc, char ***argv)
 {
     assert(*argc > 0);
@@ -37,6 +68,7 @@ char *read_file(char *name)
             long bufsize = ftell(fp);
             if (bufsize == -1)
             {
+				error_read(name);
             }
 
             source = malloc(sizeof(char) * (bufsize + 1));
@@ -48,15 +80,18 @@ char *read_file(char *name)
             size_t newLen = fread(source, sizeof(char), bufsize, fp);
             if (ferror(fp) != 0)
             {
-                fputs("Error reading file", stderr);
-            }
+                /* fputs("Error reading file", stderr); */
+				error_read(name);
+			}
             else
             {
                 source[newLen++] = '\0'; /* Just to be safe. */
             }
         }
         fclose(fp);
-    }
+    } else {
+		error_read(name);
+	}
 
     return source;
 }
@@ -84,11 +119,16 @@ int main(int argc, char **argv)
 
     if (!input_file)
     {
-        fprintf(stderr, "No file name provided. Exiting\n");
+        /* fprintf(stderr, "No file name provided. Exiting\n"); */ /* This is my personal opinion, change it if you'd like. */
+		usage(stderr, program);
         return 1;
     }
 
     char *string = read_file(input_file);
+	if (string == NULL)
+	{
+		return 1;
+	}
     vec_token_t tokens = tokenize(string);
 
     ParseValueList tree = parse(tokens);
